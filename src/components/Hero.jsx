b9813eeac016f28f78839aa05e20efd79238c166
Akkhan06 <ak01648065092@gmail.com>
2023-05-02 04:09:51 +0600

    banner section added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen w-full bg-[url('https://t4.ftcdn.net/jpg/04/89/04/65/360_F_489046512_D3W2qTasAWwtMycRQOVuQnjPFItDYxh8.webp')] bg-base-200">
+  <div className="hero-content  text-center  ">
+    <div className="max-w-full">
+      <h1 className="text-5xl text-white font-bold">Welcome to dubai chef club</h1>
+      <p className="py-6 max-w-lg mx-auto text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-success bg-yellow-400 border-0">Get Started</button>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+};
+
+export default Hero;
