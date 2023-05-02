39ae8d83c4be2ae239e2c6ea8971a5dc5a450b57
Akkhan06 <ak01648065092@gmail.com>
2023-05-02 11:44:31 +0600

    body, contact and footer secton added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import './App.css'
 import Navbar from './components/Navbar'
 // import Here from './components/Here'
 import Hero from './components/Hero'
+import Body from './components/Body/body'
+import Category from './components/Body/category'
+import SingleCards from './components/SingleCards/SingleCards'
+import Contact from './components/Contact'
+import { Footer } from 'flowbite-react'
+import Footers from './components/Footer'
 
 function App() {
 
@@ -13,6 +19,11 @@ function App() {
 
     <Navbar></Navbar>
     <Hero></Hero>
+    <Body></Body>
+
+    <SingleCards></SingleCards>
+    <Contact></Contact>
+  <Footers></Footers>
 
     </>
   )
